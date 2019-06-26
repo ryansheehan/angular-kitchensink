b752f16203f17980fa6e377ce8bae97745a75d0e
Ryan Sheehan <ryan.sheehan@tylertech.com>
2019-06-25 21:49:21 -0500

    changed project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to robot-gallery!');
+    expect(page.getTitleText()).toEqual('Welcome to character-gallery!');
   });
 
   afterEach(async () => {
